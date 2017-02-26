b371c77a57ad01bec6a15fc6bc317584230a4941
kraken <kraken@ex.ua>
2017-02-26 15:08:51 +0200

    initial

---
+++ src/main/java/com/google/training/helloworld/Reverse.java
@@ -0,0 +1,12 @@
+package com.google.training.helloworld;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+
+public class Reverse {
+    public String message;
+    public Reverse(String name) {
+        message = name.toLowerCase();
+    }
+}
